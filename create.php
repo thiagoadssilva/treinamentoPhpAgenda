97ad374a996003697fb8982c197378fb09ea2e76
Jose Da Silva <tjoses@minsait.com>
2024-05-29 05:37:58 -0300

    Criação da tela do cadastro de contato

--- create.php
+++ create.php
@@ -3,7 +3,27 @@ include("template/header.php");
 ?>
 
 
-<h1>Criando contato</h1>
+<div class="container">
+    <?php include_once("template/backbtn.html"); ?>
+    <h1 id="main-title">Criar Contato</h1>
+    <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="post">
+        <!-- Criando esse input para identificação da ação que vamos ter no arquivo process.php -->
+        <input type="hidden" name="type" value="create">
+        <div class="form-group">
+            <label for="name">Nome do contato:</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Digite seu nome aqui" required>
+        </div>
+        <div class="form-group">
+            <label for="phone">Telefone do contato:</label>
+            <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite seu telefone aqui" required>
+        </div>
+        <div class="form-group">
+            <label for="observations">Observações:</label>
+            <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Digite sua observação aqui" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>
 
 
 <?php
