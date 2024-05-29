f2b5ef1dd5336da7b30212ee49ab445cbdd973e5
Jose Da Silva <tjoses@minsait.com>
2024-05-29 04:52:25 -0300

    Finalização da home e pegando as informações do banco

--- index.php
+++ index.php
@@ -12,7 +12,28 @@ include("template/header.php");
 
     <h1 id="main-title">Minha Agenda</h1>
     <?php if (count($contacts) > 0) : ?>
-        <p>Tem contatos</p>
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <?php foreach($contacts as $contact): ?>
+                <tr>
+                    <td scope="rwo" class="col-id"><?= $contact["id"] ?></td>
+                    <td scope="rwo"><?= $contact["name"] ?></td>
+                    <td scope="rwo"><?= $contact["phone"] ?></td>
+                    <td class="actions">
+                        <a href=""><i class="fas fa-eye check-icon"></i></a>
+                        <a href=""><i class="far fa-edit edit-icon"></i></a>
+                        <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                    </td>
+                </tr>
+            <?php endforeach ?>    
+        </table>
     <?php else : ?>
         <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar</a>.</p>
     <?php endif ?>
